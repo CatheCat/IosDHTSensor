8dc6ea6abe5cabfe58ab0fbc104370cec184b957
Catherine <Catherine@CatherinedeMacBook-Air.local>
2016-11-23 21:11:21 +0800

    Create project

---
+++ DHTSensorSample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DHTSensorSample
+//
+//  Created by Catherine on 2016/11/23.
+//  Copyright © 2016年 Catherine. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DHTSensorSample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DHTSensorSample
+//
+//  Created by Catherine on 2016/11/23.
+//  Copyright © 2016年 Catherine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
